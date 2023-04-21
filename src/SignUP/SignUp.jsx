975c07d5bca0a6e67894743315705d4557d1da7c
Fahim Shariar <fahimshariar28@gmail.com>
2023-04-21 16:29:29 +0600

    Login And Signup page design

--- src/SignUP/SignUp.jsx
+++ src/SignUP/SignUp.jsx
@@ -1,10 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import "./SignUp.css";
+import { Link } from "react-router-dom";
 
 const SignUp = () => {
+  const [error, setError] = useState("");
+
+  const handelSignUp = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const email = form.email.value;
+    const password = form.password.value;
+    const confirm = form.confirm.value;
+    console.log(email, password, confirm);
+    if (password !== confirm) {
+      setError("Please Enter the Same password");
+      return;
+    } else if (password.length < 6) {
+      setError("Password Must be 6 characters or more");
+      return;
+    }
+  };
+
   return (
-    <div>
-      <h2>Please Sign Up</h2>
+    <div className="form-container">
+      <h2 className="form-title">Sign Up</h2>
+      <form onSubmit={handelSignUp}>
+        <div className="form-control">
+          <label htmlFor="email">Email</label>
+          <input type="email" name="email" id="" required />
+        </div>
+        <div className="form-control">
+          <label htmlFor="password">Password</label>
+          <input type="password" name="password" id="" required />
+        </div>
+        <div className="form-control">
+          <label htmlFor="confirm">Confirm Password</label>
+          <input type="password" name="confirm" id="" required />
+        </div>
+        <input className="btn-submit" type="submit" value="Sign Up" />
+      </form>
+      <p>
+        <small>
+          Already have an account? <Link to="/login">Login</Link>
+        </small>
+      </p>
+      <p className="text-error">{error}</p>
     </div>
   );
 };
--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,10 +1,27 @@
 import React from "react";
 import "./Login.css";
+import { Link } from "react-router-dom";
 
 const Login = () => {
   return (
-    <div>
-      <h3>Login Page</h3>
+    <div className="form-container">
+      <h2 className="form-title">Login</h2>
+      <form>
+        <div className="form-control">
+          <label htmlFor="email">Email</label>
+          <input type="email" name="email" id="" required />
+        </div>
+        <div className="form-control">
+          <label htmlFor="password">Password</label>
+          <input type="password" name="password" id="" required />
+        </div>
+        <input className="btn-submit" type="submit" value="Login" />
+      </form>
+      <p>
+        <small>
+          New to Ema-john? <Link to="/signup">Create New Account</Link>
+        </small>
+      </p>
     </div>
   );
 };
